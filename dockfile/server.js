c332966796bcb3ddbefab8f21d2ac5d2f28863a6
Prakash <prakashghadage025@gmail.com>
2023-01-01 20:06:10 +0530

    first-add

---
+++ dockfile/server.js
@@ -0,0 +1,11 @@
+const express=require('express');
+
+const bodyparser=require('body-parser');
+ const app=express();
+
+ app.use(bodyparser.urlencoded({extended:false}));
+ app.get('/',(req,res)=>{
+    res.sendFile("/index.html",{root:__dirname});
+ })
+ app.listen(3000);
+ console.log("server running on port 3000");
